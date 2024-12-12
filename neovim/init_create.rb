d242c1f5a8a2293aa774cb3a10ec4d8a3636df51
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-12-12 11:52:08 +0900

    Create, don't use.

---
+++ neovim/init_create.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'open3'
+require 'fileutils'
+
+Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
+
+# create init.vim
+class White
+  def self.magic
+    if Dir.exist?(File.expand_path('~/AppData/Local/nvim'))
+      puts 'Already have a  folder...do nothing.'
+    else
+      myhome = File.expand_path('~/AppData/Local/nvim/init.vim')
+      File.open(myhome, 'a:utf-8', perm=0o777) do |f|
+      f.puts <<~DOC
+" .netfile
+if filereadable(expand('~/.netfile/lua/init.lua'))
+  lua require('init')
+endif
+      DOC
+      puts 'I placed init.vim in the specified PATH.'
+    end
+    end
+  end
+end
+
+begin
+  White.magic
+rescue StandardError => e
+  puts e.backtrace
+ensure
+  GC.compact
+end
+
+__END__
+
